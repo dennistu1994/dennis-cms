06fd29a885d2fe2630c552b827c2836ff99f9f4e
Dennis Tu <dennistu1994@hotmail.com>
2015-09-29 13:47:28 -0400

    temp

--- server.js
+++ server.js
@@ -1,71 +1,8 @@
-var http = require('http');
-var path = require('path');
-var log = require('./utils/log');
-var util = require('util');
-var fs = require('fs');
-
-var Init = require('./init');
-var Index = Init.init_modules();
-
-var server = http.createServer(function(req, res) {
-  //log(JSON.stringify(util.inspect(req)), "logs/req_" + Date.now() + ".json");
-  var url = req.url;
-  var req_url = req.url;
-  if(req_url.indexOf("/index") !== 0){
-    req_url = "/index" + req_url;
-  }
-  try {
-    var fstats = fs.lstatSync('public'+url);
-    if(fstats.isFile()){
-      //file exists, serve it
-      serve_file(url, res);
-    } else {
-      Index.route(req, res, req_url);
-    }
-  } catch (e){
-    Index.route(req, res, req_url);
-  }
+var express = require('express');
+var app = express();
+app.use(express.static('public'));
+var port = process.env.PORT || 8000;
+var server = app.listen(port, function () {
+  console.log("server info:");
+  console.log(server.address());
 });
-
-server.listen(process.env.PORT || 8000, function(){
-  console.log('listening port',process.env.PORT || 8000);
-});
-
-function serve_file(url, res){
-  var extname = path.extname(url);
-  var contentType;
-  switch (extname) {
-    case '.ico':
-      contentType = "image/x-icon";
-      break;
-    case '.js':
-      contentType = 'text/javascript';
-      break;
-    case '.css':
-      contentType = 'text/css';
-      break;
-    case '.json':
-      contentType = 'application/json';
-      break;
-    case '.png':
-      contentType = 'image/png';
-      break;
-    case '.jpg':
-      contentType = 'image/jpg';
-      break;
-    case '.wav':
-      contentType = 'audio/wav';
-      break;
-    default:
-      contentType = 'text/html';
-  }
-  res.writeHead(200, { 'Content-Type': contentType });
-  fs.readFile('public'+url, function(error, content) {
-    if (error) {
-      res.status(500).end();
-    }
-    else {
-        res.end(content, 'utf-8');
-    }
-  });
-}
