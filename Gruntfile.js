7a1ea23d81b0feb4371cbcaa6a251b734854a097
Dennis Tu <jq2tu@uwaterloo.ca>
2015-10-14 17:25:11 -0400

    old

---
+++ Gruntfile.js
@@ -0,0 +1,13 @@
+module.exports = function(grunt) {
+
+  grunt.initConfig({
+
+  });
+
+  grunt.loadNpmTasks('grunt-contrib-jasmine');
+
+  grunt.registerTask('default', function(){
+    console.log('no default task');
+  });
+
+};
